5b6151315f5f5d13fb204e422d2dc203d39055dd
Stephen Hellicar <shellicar@gmail.com>
2025-01-09 20:10:35 +1100

    Fix client principal type (#17)
    * Update Client Principal for app services
    
    * Move badges to main README

--- packages/svelte-adapter-azure-functions/src/types.ts
+++ packages/svelte-adapter-azure-functions/src/types.ts
@@ -1,6 +1,31 @@
+// https://learn.microsoft.com/en-us/azure/app-service/configure-authentication-user-identities#decoding-the-client-principal-header
+
+export type ClientPrincipalClaim = {
+  /**
+   * Type
+   */
+  typ: string;
+  /**
+   * Value
+   */
+  val: string;
+};
+
 export type ClientPrincipal = {
-  identityProvider: string;
-  userId: string;
-  userDetails: string;
-  userRoles: string[];
+  /**
+   * Identity Provider
+   */
+  auth_typ: string;
+  /**
+   * Claims
+   */
+  claims: ClientPrincipalClaim[];
+  /**
+   * Name Claim Type
+   */
+  name_typ: string;
+  /**
+   * Role Claim Type
+   */
+  role_typ: string;
 };
